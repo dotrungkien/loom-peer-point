27d01361b502206a070b0608f440c95c05d112d0
Do Trung Kien <trungkien.keio@gmail.com>
2018-10-12 09:45:45 +0700

    update

--- truffle.js
+++ truffle.js
@@ -9,8 +9,14 @@ const writeUrl = 'http://127.0.0.1:46658/rpc'
 const readUrl = 'http://127.0.0.1:46658/query'
 const privateKey = readFileSync('./private_key', 'utf-8')
 
-const loomTruffleProvider = new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey)
+const loomTruffleProvider = new LoomTruffleProvider(
+  chainId,
+  writeUrl,
+  readUrl,
+  privateKey
+)
 loomTruffleProvider.createExtraAccounts(10)
+
 module.exports = {
   contracts_build_directory: join(__dirname, './src/contracts'),
   networks: {
