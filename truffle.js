b7be15a651b01922a7d14f02c039b27c4b98c8b2
Do Trung Kien <trungkien.keio@gmail.com>
2018-10-12 10:04:31 +0700

    update config

--- truffle.js
+++ truffle.js
@@ -2,20 +2,7 @@
 require('babel-register')
 const { readFileSync } = require('fs')
 const { join } = require('path')
-const LoomTruffleProvider = require('loom-truffle-provider/dist')
-
-const chainId = 'default'
-const writeUrl = 'http://127.0.0.1:46658/rpc'
-const readUrl = 'http://127.0.0.1:46658/query'
-const privateKey = readFileSync('./private_key', 'utf-8')
-
-const loomTruffleProvider = new LoomTruffleProvider(
-  chainId,
-  writeUrl,
-  readUrl,
-  privateKey
-)
-loomTruffleProvider.createExtraAccounts(10)
+const LoomTruffleProvider = require('loom-truffle-provider')
 
 module.exports = {
   contracts_build_directory: join(__dirname, './src/contracts'),
@@ -26,7 +13,24 @@ module.exports = {
       network_id: '*' // Match any network id
     },
     loom: {
-      provider: loomTruffleProvider,
+      provider: () => {
+        const privateKey = readFileSync('./private_key', 'utf-8')
+        const chainId = 'default'
+        const writeUrl = 'http://127.0.0.1:46658/rpc'
+        const readUrl = 'http://127.0.0.1:46658/query'
+
+        const loomTruffleProvider = new LoomTruffleProvider(
+          chainId,
+          writeUrl,
+          readUrl,
+          privateKey
+        )
+        loomTruffleProvider.createExtraAccountsFromMnemonic(
+          'gravity top burden flip student usage spell purchase hundred improve check genre',
+          10
+        )
+        return loomTruffleProvider
+      },
       network_id: '*'
     }
   }
