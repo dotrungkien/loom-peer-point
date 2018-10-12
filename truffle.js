d8ed793563a2e0ea7c398346b92e593a29e878b3
Do Trung Kien <trungkien.keio@gmail.com>
2018-10-12 10:08:00 +0700

    update migrate

--- truffle.js
+++ truffle.js
@@ -1,5 +1,6 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
+const path = require('path')
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const LoomTruffleProvider = require('loom-truffle-provider')
@@ -14,7 +15,10 @@ module.exports = {
     },
     loom: {
       provider: () => {
-        const privateKey = readFileSync('./private_key', 'utf-8')
+        const privateKey = readFileSync(
+          path.join(__dirname, './private_key'),
+          'utf-8'
+        )
         const chainId = 'default'
         const writeUrl = 'http://127.0.0.1:46658/rpc'
         const readUrl = 'http://127.0.0.1:46658/query'
